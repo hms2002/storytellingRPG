9b76d77e67a0ff88c7e3d901920b428fe057a501
nadoman354 <nadoman354@naver.com>
2024-07-09 13:07:28 +0900

    속기사의검

---
+++ Assets/Scripts/KeywordMain/FastKnightSword.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastKnightSword : KeywordMain
+{
+    public override void Execute(Actor caster, Actor target, Sentence sentence)
+    {
+        sentence.SetXRepeat(2);
+        sentence.DamageControl(2);
+    }
+}
