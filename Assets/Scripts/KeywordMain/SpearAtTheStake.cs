5030e5c9ef0e8273725815e9952b34d26580ff85
hms2002 <hmm2002@naver.com>
2024-07-09 14:56:10 +0900

    SpearAtTheStake

---
+++ Assets/Scripts/KeywordMain/SpearAtTheStake.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearAtTheStake : KeywordMain
+{
+    public override void Execute(Actor caster, Actor target, Sentence sentence)
+    {
+        target.Damaged( target.GetBurnStack() , DamageType.Beat);
+    }
+    public override void Check(KeywordSup _keywordSup)
+    {
+
+    }
+}
