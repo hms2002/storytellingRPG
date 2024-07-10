eaf61db37fc85af3c71c789d0517bbe9bd6ba032
LHS <gustjs1234596@gmail.com>
2024-07-10 23:43:37 +0900

    공부하고 확실히 만들예정 (닷트윈 적용, 선과 노드 작업 진행중)

---
+++ Assets/LHS_MapTest/UIRenderer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRenderer : MonoBehaviour
+{
+    private enum SegmentType
+    {
+        Start,
+        Middle,
+        End,
+        Full,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
