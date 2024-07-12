c69a1f2b65fefcdb698687d640a6d69a12bdfb31
hms2002 <hmm2002@naver.com>
2024-07-12 15:35:26 +0900

    버그 수정

--- Assets/Scripts/KeywordMain/SpearAtTheStake.cs
+++ Assets/Scripts/KeywordMain/SpearAtTheStake.cs
@@ -7,7 +7,7 @@ public class SpearAtTheStake : KeywordMain
     
     public override void Execute(Actor caster, Actor target, Sentence sentence)
     {
-        target.Damaged( target.burnStack , DamageType.Beat);
+        target.Damaged(caster, target.burnStack , DamageType.Beat);
         sentence.damage += target.burnStack;
     }
     public override void Check(KeywordSup _keywordSup)
