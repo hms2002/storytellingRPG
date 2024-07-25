dfe92a6d22a551bd772d7b40bdaad0a37b8bdc11
Panabolic <4097lsh@naver.com>
2024-07-25 16:20:06 +0900

    책 페이지 넘기기 일단 40% 완성

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -6,8 +6,9 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager uIManager;
 
-    [Header("캔버스")]
-    [SerializeField] private GameObject stateCanvas;
+    private GameObject playerPrefab;
+
+    [Header("UI 캔버스")]
     [SerializeField] private GameObject keywordCanvas;
     [SerializeField] private GameObject textBoxCanvas;
 
@@ -34,8 +35,20 @@ public class UIManager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
 
+    /// <summary>
+    /// 전장 탈주 시 UI 세팅 및 책 애니메이션 재생 메소드
+    /// </summary>
     private void GetOutOfBattleField()
     {
-        stateCanvas.SetActive(false);
-    }
+        // 플레이어 프리팹 
+        playerPrefab = GameObject.FindGameObjectWithTag("Player");
+
+        // 플레이어 프리팹 삭제
+        Destroy(playerPrefab);
+
+        // 전투 관련 캔버스 끄기
+        keywordCanvas.SetActive(false);
+        textBoxCanvas.SetActive(false);
+
+    } // 긴장도, 플레이어 체력 등은 전투 돌입 시 초기화하도록 당부함
 };
