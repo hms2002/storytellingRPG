960885d30836a5a7541e0f71c53056df35c0d23e
hms2002 <hmm2002@naver.com>
2024-07-26 11:31:28 +0900

    보상 시스템 완성~

--- Assets/Scripts/Manager/RewardManager.cs
+++ Assets/Scripts/Manager/RewardManager.cs
@@ -6,26 +6,50 @@ public class RewardManager : MonoBehaviour
 {
     [SerializeField] private Actor player;
     public static RewardManager instance;
-    List<List<GameObject>> rewardPrefabSets = new List<List<GameObject>>();
-    [SerializeField] List<GameObject> rewardSet1;
+    public GameObject rewardCanvas;
+
+    public List<Transform> rewordPivot;
+
+    // 정보를 채워넣을 껍데기
+    [SerializeField] GameObject rewardOffset;
+
+    [Header("키워드 프리펩 넣으면 보상으로 나오게 됨")]
+    [SerializeField] List<GameObject> rewardKeywords;
     private void Awake()
     {
         if (instance != null) Destroy(this);
         instance = this;
+        rewardCanvas.SetActive(false);
     }
 
-    public void  SwowRewards()
+    // 아직은 단순히 리스트의 0~2번째 소스를 보상으로 만들었음. 랜덤으로 바꿔야 함
+    public void  ShowRewards()
     {
-
+        // 보상 UI 띄우기
+        rewardCanvas.SetActive(true);
+        // 보상 버튼 생성
+        GameObject rewardInstance1 = Instantiate(rewardOffset, rewordPivot[0].transform.position, Quaternion.identity, rewardCanvas.transform);
+        // 보상 데이터 채우기
+        rewardInstance1.GetComponent<Reward>().SettingReward(rewardKeywords[0]);
+        // 반복
+        GameObject rewardInstance2 = Instantiate(rewardOffset, rewordPivot[1].transform.position, Quaternion.identity, rewardCanvas.transform);
+        rewardInstance2.GetComponent<Reward>().SettingReward(rewardKeywords[1]);
+        
+        GameObject rewardInstance3 = Instantiate(rewardOffset, rewordPivot[2].transform.position, Quaternion.identity, rewardCanvas.transform);
+        rewardInstance3.GetComponent<Reward>().SettingReward(rewardKeywords[2]);
     }
 
     public void AddMainKeywordToDeck(GameObject _keywordmain)
     {
         player.AddMainKeywordToOriginalDeck(_keywordmain);
+        rewardCanvas.SetActive(false);
+        GameManager.instance.EndSelectReward();
     }
 
     public void AddSupKeywordToDeck(GameObject _keywordSup)
     {
         player.AddSupKeywordToOriginalDeck(_keywordSup);
+        rewardCanvas.SetActive(false);
+        GameManager.instance.EndSelectReward();
     }
 }
--- Assets/Scripts/Reward.cs
+++ Assets/Scripts/Reward.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Reward : MonoBehaviour
 {
-    public GameObject keywardPrefab;
+    public GameObject keywordPrefab;
+    public TextMeshProUGUI rewardNameText;
+    public Button button;
+    public void SettingReward(GameObject _keywordPrefab)
+    {
+        keywordPrefab = _keywordPrefab;
 
+        // 정보를 가져오기 위해 인스턴스화
+        GameObject temp = Instantiate(keywordPrefab);
+        temp.SetActive(false);
 
+        // 텍스트 정보, 키워드 프리펩 정보 가져오기
+        rewardNameText.text = temp.transform.GetComponentInChildren<TextMeshProUGUI>().text;
+        
+            // 메인 키워드면 AddThisToMainDeck()를 본인 버튼 이벤트에 추가
+        if (temp.GetComponent<KeywordMain>() != null)
+            button.onClick.AddListener(AddThisToMainDeck);
+            // 서포트 키워드면 AddThisToSupDeck()를 본인 버튼 이벤트에 추가
+        else if (temp.GetComponent<KeywordSup>() != null)
+            button.onClick.AddListener(AddThisToSupDeck);
+        
+        // 정보 취하고 삭제
+        Destroy(temp);
+    }
     public void AddThisToMainDeck()
     {
-        RewardManager.instance.AddMainKeywordToDeck(keywardPrefab);
+        RewardManager.instance.AddMainKeywordToDeck(keywordPrefab);
     }
     public void AddThisToSupDeck()
     {
-        RewardManager.instance.AddSupKeywordToDeck(keywardPrefab);
+        RewardManager.instance.AddSupKeywordToDeck(keywordPrefab);
     }
 }
