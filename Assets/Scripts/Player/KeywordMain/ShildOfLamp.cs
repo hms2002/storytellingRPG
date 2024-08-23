8fab0031d64c120c4a7262b4b4b9d3da2c1e6265
nadoman354 <nadoman354@naver.com>
2024-08-23 22:41:38 +0900

    Update ShildOfLamp.cs

--- Assets/Scripts/Player/KeywordMain/ShildOfLamp.cs
+++ Assets/Scripts/Player/KeywordMain/ShildOfLamp.cs
@@ -14,6 +14,7 @@ public class ShildOfLamp : KeywordMain
 
     public override void Execute(Actor caster, Actor target)
     {
+        caster.protect += keywordProtect;
         target.charactorState.AddState(StateType.burn, buffStack);
     }
 
