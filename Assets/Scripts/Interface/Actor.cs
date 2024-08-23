8f9d8e7e5e993362ee3f19928e5eca5242712be4
hms2002 <hmm2002@naver.com>
2024-08-23 16:57:10 +0900

    다시

--- Assets/Scripts/Interface/Actor.cs
+++ Assets/Scripts/Interface/Actor.cs
@@ -156,7 +156,7 @@ public class Actor : MonoBehaviour
                 if (charactorState.GetStateStack(StateType.ore) != 0 && value < _hp)
                     charactorState.ReductionByValue(StateType.ore, _hp - value);
 
-            if(value > _hp && value == MAX_HP)
+            if(value > _hp && value != MAX_HP)
                 UIManager.instance.ActiveDamageText(transform.position, value-_hp, Color.green);
 
 
@@ -299,7 +299,6 @@ public class Actor : MonoBehaviour
         charactorState.Init(stateUIController);
         charactorState.actor = this;
 
-        _hp = _MAX_HP;
         _protect = 0;
         _heal = 0;
         _damage = 0;
